b3a6ab1e5eae6006e58ec837ef156be78aace7ca
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-25 13:41:59 +0200

    exo 1 p 1 php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 1</title>
+  </head>
+  <body>
+    <?php
+    $name = 'lamotte';
+    echo $name;
+     ?>
+  </body>
+</html>
