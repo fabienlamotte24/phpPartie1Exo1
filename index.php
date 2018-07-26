ac7604998e6721b490afc8d8645ae5c35f91fd49
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-26 15:22:42 +0200

    mise en commentaire des exercices

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <title>Exercice 1</title>
   </head>
   <body>
-    <p>
-      <?php
-        $name = 'lamotte';
-        echo $name;
-      ?>
+    <p> <!--balise de texte html -->
+      <?php /*balise ouverte de php*/
+        $name = 'lamotte'; /*déclaration de la variable*/
+        echo $name;/*on demande avec echo d'afficher la variable*/
+      ?><!--On ferme la balise php-->
     </p>
   </body>
 </html>
